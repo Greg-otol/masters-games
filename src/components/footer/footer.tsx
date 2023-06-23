1f368b7623f1fc71207bfdf999ec18403d325138
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-06-23 18:35:36 -0300

    chore: create footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CopyRight } from "../footer/style-footer";
+
+export const Footer: React.FC = () => {
+  return (
+    <>
+      <CopyRight>© 2023 Desenvolvido por OtOl_Tech</CopyRight>
+    </>
+  );
+};
