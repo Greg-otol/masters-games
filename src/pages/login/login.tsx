67a2956322e85668e5463ab1230959b4d436ce80
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-08 17:46:54 -0300

    feat: create login

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,84 @@
+import {
+  LoginA,
+  LoginButton,
+  LoginContainer,
+  LoginDiv,
+  LoginForm,
+  LoginImg,
+  LoginInput,
+  LoginLabel,
+  LoginP,
+  LoginTitle,
+} from "./styleLogin";
+import { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../services/firebase";
+import { Loading } from "../../components/loading/loading";
+import logo from "../../assets/img/logo.png";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  function handleSignIn(event: React.ChangeEvent<HTMLInputElement> | any) {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate("/");
+      } else {
+        navigate("/register");
+      }
+    });
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <LoginContainer>
+      <LoginImg src={logo} alt="Imagem da logo AppMasters" />
+      <LoginTitle>Digite suas informações de login</LoginTitle>
+
+      <LoginForm>
+        <LoginLabel htmlFor="email">E-mail</LoginLabel>
+        <LoginInput
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <LoginLabel htmlFor="password">Senha</LoginLabel>
+        <LoginInput
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Digite sua senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <LoginA href="#">Esqueceu sua senha ?</LoginA>
+
+        <LoginButton className="button" onClick={handleSignIn}>
+          Entrar
+        </LoginButton>
+        <LoginDiv>
+          <LoginP>Você não tem uma conta?</LoginP>
+          <Link to="/register">
+            <LoginP>Clique aqui</LoginP>
+          </Link>
+        </LoginDiv>
+      </LoginForm>
+    </LoginContainer>
+  );
+}
---
+++ src/pages/login/styleLogin.ts
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { theme } from "../../styleGlobal";
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const LoginImg = styled.img`
+  width: 80px;
+`;
+
+export const LoginTitle = styled.h2`
+  padding: 20px;
+  text-align: center;
+  color: ${theme.colors.tertiary};
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+
+  @media (max-width: 720px) {
+    width: 100%;
+  }
+`;
+
+export const LoginLabel = styled.label`
+  width: 80%;
+  color: ${theme.colors.tertiary};
+`;
+
+export const LoginInput = styled.input`
+  width: 80%;
+  height: 40px;
+  border-radius: 10px;
+  padding-left: 10px;
+  border: 1px solid ${theme.colors.primary};
+  outline: none;
+
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px white inset;
+  }
+`;
+
+export const LoginA = styled.a`
+    width: 80%;
+    text-decoration: none;
+    color: ${theme.colors.tertiary};
+    cursor: pointer;
+    &:hover {
+        color: ${theme.colors.primary};
+    }
+`;
+
+export const LoginButton = styled.button`
+  width: 80%;
+  height: 40px;
+  font-size: 20px;
+  color: ${theme.colors.secondary};
+  margin: 10px 0 5px 0;
+  background-color: ${theme.colors.primary};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+export const LoginDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  width: 80%;
+  gap: 10px;
+
+  @media (max-width: 385px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const LoginP = styled.p`
+  text-decoration: none;
+  color: ${theme.colors.tertiary};
+`;
