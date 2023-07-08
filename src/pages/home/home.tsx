115a461be8b08c28316fe6e91ac64b0431eeca84
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-08 17:54:37 -0300

    chore: implemented favorite and rate

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,20 @@
 import {
   Card,
   Cards,
+  DivStarFavorite,
+  Favorities,
   HomeContainer,
   HomeDeveloped,
   HomeGenre,
   HomeImg,
   HomeP,
   HomeSearchContainer,
+  OrdersButton,
+  OrdersDiv,
+  Star,
+  StarDiv,
   Title,
+  UserP,
 } from "./styleHome";
 import { useEffect, useRef, useState } from "react";
 import { getAllGames } from "../../services/servicesGames/getAllGames";
@@ -18,6 +25,21 @@ import { GenrerSelect } from "../../components/search/searchGenre/searchGenre";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "../../components/footer/footer";
 import { Header } from "../../components/header/header";
+import heartGray from "../../assets/img/heart-gray.png";
+import heartRed from "../../assets/img/heart-red.png";
+import {
+  collection,
+  doc,
+  setDoc,
+  deleteDoc,
+  query,
+  getDocs,
+  where,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
+import { auth, firestore } from "../../services/firebase";
+import { Favorites } from "../../components/favorites/favorites";
 
 export function Home() {
   const navigate = useNavigate();
@@ -26,6 +48,10 @@ export function Home() {
   const [searchGenre, setSearchGenre] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const timerRef = useRef<number | null>(null);
+  const [favorites, setFavorites] = useState<IGames[]>([]);
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [ratings, setRatings] = useState<{ [id: string]: number }>({});
+  const [sortByRating, setSortByRating] = useState<boolean>(false);
 
   useEffect(() => {
     const loadGames = async () => {
@@ -83,22 +109,205 @@ export function Home() {
     return true;
   });
 
+  useEffect(() => {
+    const loadFavorites = async () => {
+      const favoritesQuery = query(collection(firestore, "favorites"));
+      const favoritesSnapshot = await getDocs(favoritesQuery);
+      const favoriteGames: IGames[] = [];
+      favoritesSnapshot.forEach((doc) => {
+        const favoriteGame = doc.data() as IGames;
+        favoriteGames.push(favoriteGame);
+      });
+      setFavorites(favoriteGames);
+    };
+
+    loadFavorites();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(firestore, "favorites"),
+      (snapshot) => {
+        const favoriteGames: IGames[] = [];
+        snapshot.forEach((doc) => {
+          const favoriteGame = doc.data() as IGames;
+          favoriteGames.push(favoriteGame);
+        });
+        setFavorites(favoriteGames);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleFavorite = async (game: IGames) => {
+    try {
+      const isFavorite = favorites.find((favorite) => favorite.id === game.id);
+      if (auth.currentUser) {
+        if (isFavorite) {
+          const favoriteDocQuery = query(
+            collection(firestore, "favorites"),
+            where("id", "==", game.id)
+          );
+          const favoriteDocs = await getDocs(favoriteDocQuery);
+          favoriteDocs.forEach(async (doc) => {
+            await deleteDoc(doc.ref);
+          });
+          setFavorites(favorites.filter((favorite) => favorite.id !== game.id));
+        } else {
+          const favoriteGame = { ...game };
+          const favoriteDoc = doc(collection(firestore, "favorites"));
+          await setDoc(favoriteDoc, favoriteGame);
+          setFavorites([...favorites, favoriteGame]);
+        }
+      } else {
+        console.log("Não está logado!");
+        navigate("/auth");
+      }
+    } catch (error) {
+      console.error("Erro ao favoritar o jogo:", error);
+    }
+  };
+
+  const isFavorite = (game: IGames) => {
+    return favorites.some((favorite) => favorite.id === game.id);
+  };
+
+  const handleToggleFavorites = () => {
+    setShowFavorites(!showFavorites);
+  };
+
+  const handleRating = (gameId: number, rating: number) => {
+    if (auth.currentUser) {
+      setRatings((prevRatings) => ({
+        ...prevRatings,
+        [gameId.toString()]: rating,
+      }));
+    } else {
+      console.log("Não está logado!");
+      navigate("/auth");
+    }
+  };
+
+  useEffect(() => {
+    // Recuperar as classificações do Firestore ao carregar a página
+    const fetchRatings = async () => {
+      try {
+        const db = getFirestore();
+        const ratingsCollection = collection(db, "ratings");
+        const ratingsSnapshot = await getDocs(ratingsCollection);
+        const ratingsData: { [id: string]: number } = {};
+        ratingsSnapshot.forEach((doc) => {
+          ratingsData[doc.id] = doc.data().rating;
+        });
+        setRatings(ratingsData);
+      } catch (error) {
+        console.error("Erro ao recuperar as classificações:", error);
+      }
+    };
+    fetchRatings();
+  }, []);
+
+  useEffect(() => {
+    // Salvar as classificações no Firestore sempre que forem alteradas
+    const saveRatings = async () => {
+      try {
+        const db = getFirestore();
+        const ratingsCollection = collection(db, "ratings");
+        Object.entries(ratings).forEach(([gameId, rating]) => {
+          const gameDoc = doc(ratingsCollection, gameId);
+          setDoc(gameDoc, { rating });
+        });
+      } catch (error) {
+        console.error("Erro ao salvar as classificações:", error);
+      }
+    };
+    saveRatings();
+  }, [ratings]);
+
+  const handleSortByRating = () => {
+    setSortByRating((prevSortByRating) => !prevSortByRating);
+  };
+
+  useEffect(() => {
+    // Ordenar os jogos com base nas classificações
+    const sortedGames = [...games].sort((a, b) => {
+      const ratingA = ratings[a.id];
+      const ratingB = ratings[b.id];
+
+      if (ratingA === undefined && ratingB === undefined) {
+        return 0;
+      } else if (ratingA === undefined) {
+        return 1;
+      } else if (ratingB === undefined) {
+        return -1;
+      }
+
+      return sortByRating ? ratingB - ratingA : ratingA - ratingB;
+    });
+
+    setGames(sortedGames);
+  }, [ratings, sortByRating]);
+
   return (
     <HomeContainer>
       <Header />
+      {auth.currentUser ? (
+        <UserP>Usuário logado: {auth.currentUser.email}</UserP>
+      ) : (
+        <UserP>Usuário não logado</UserP>
+      )}
       <HomeSearchContainer>
         <SearchGames value={search} handleSearchValue={changeSearchGames} />
         <GenrerSelect valueMap={genre} handleSearchValue={changeSearchGenre} />
       </HomeSearchContainer>
+      <OrdersDiv>
+        <OrdersButton onClick={handleToggleFavorites}>
+          {showFavorites ? "Mostrar Todos" : "Mostrar Favoritos"}
+        </OrdersButton>
+        <OrdersButton onClick={handleSortByRating}>
+          {sortByRating
+            ? "Ordenar por menores avaliações"
+            : "Ordenar por maiores avaliações"}
+        </OrdersButton>
+      </OrdersDiv>
       {isLoading ? (
         <Loading />
       ) : filteredGames.length === 0 ? (
         <HomeP>Nada encontrado com esse termo!</HomeP>
+      ) : showFavorites ? (
+        <Favorites
+          favorites={favorites}
+          allGames={filteredGames}
+          handleFavorite={handleFavorite}
+          handleRating={handleRating}
+          ratings={ratings}
+        />
       ) : (
         <Cards>
           {filteredGames.map((game) => {
             return (
               <Card key={game.id}>
+                <DivStarFavorite>
+                  <Favorities
+                    onClick={() => handleFavorite(game)}
+                    src={isFavorite(game) ? heartRed : heartGray}
+                    alt="Imagem de um coração"
+                  />
+                  <StarDiv>
+                    {[1, 2, 3, 4].map((value) => (
+                      <Star
+                        key={value}
+                        active={value <= (ratings[game.id] || 0)}
+                        onClick={() => handleRating(game.id, value)}
+                      >
+                        ★
+                      </Star>
+                    ))}
+                  </StarDiv>
+                </DivStarFavorite>
                 <Title>{game.title}</Title>
                 <HomeImg src={game.thumbnail} alt="Imagem da capa do jogo" />
                 <HomeDeveloped>Por: {game.developer}</HomeDeveloped>
--- src/pages/home/styleHome.ts
+++ src/pages/home/styleHome.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { theme } from "../../styleGlobal";
 
 export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   min-height: 100vh;
   padding-top: 80px;
 `;
@@ -11,6 +13,7 @@ export const HomeSearchContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  width: 100%;
   padding: 20px 50px 0 50px;
   gap: 10px;
 
@@ -38,12 +41,13 @@ export const Card = styled.div`
   justify-content: center;
   flex-direction: column;
   padding: 10px;
-  border: 1px solid rgb(172, 0, 0);
+  border: 1px solid ${theme.colors.primary};
   border-radius: 5px;
 `;
 
 export const Title = styled.h2`
   text-align: center;
+  color: ${theme.colors.tertiary};
 `;
 
 export const HomeImg = styled.img`
@@ -54,18 +58,83 @@ export const HomeGenre = styled.p`
   font-weight: bold;
   margin-top: 5px;
   text-align: center;
+  color: ${theme.colors.tertiary};
 `;
 
 export const HomeDeveloped = styled.p`
   font-weight: bold;
   margin-top: 5px;
   text-align: center;
+  color: ${theme.colors.tertiary};
 `;
 
 export const HomeP = styled.p`
   font-size: 18px;
   font-weight: bold;
   text-align: center;
-  color: rgba(172, 0, 0, 0.8);
+  color: ${theme.colors.primary};
   padding-top: 150px;
+`;
+
+export const OrdersDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  gap: 10px;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+    width: 100%;
+    gap: 0;
+  }
+`;
+
+export const OrdersButton = styled.button`
+  width: 80%;
+  height: 40px;
+  font-size: 16px;
+  color: ${theme.colors.secondary};
+  margin: 10px 0 5px 0;
+  background-color: ${theme.colors.primary};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+export const DivStarFavorite = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Favorities = styled.img`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  color: black;
+`;
+
+interface StarProps {
+  active: boolean;
+}
+
+export const StarDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const StarP = styled.p``;
+
+export const Star = styled.span<StarProps>`
+  color: ${(props) => (props.active ? "#ffd000" : "gray")};
+  cursor: pointer;
+  font-size: 35px;
+`;
+
+export const UserP = styled.p`
+  color: ${theme.colors.primary};
+  text-align: center;
+  padding-top: 10px;
 `;
