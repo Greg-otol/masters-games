17534964a81a625e06d810a4255ec9414d621255
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-08 17:47:36 -0300

    feat: create register

---
+++ src/pages/register/register.tsx
@@ -0,0 +1,78 @@
+import {
+  RegisterButton,
+  RegisterContainer,
+  RegisterDiv,
+  RegisterForm,
+  RegisterInput,
+  RegisterLabel,
+  RegisterP,
+  RegisterTitle,
+} from "./styleRegister";
+import { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../services/firebase";
+import { Loading } from "../../components/loading/loading";
+import logo from "../../assets/img/logo.png";
+import { LoginImg } from "../login/styleLogin";
+
+export function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  function handleSignOut(event: React.ChangeEvent<HTMLInputElement> | any) {
+    event.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate("/auth");
+      } else {
+        return;
+      }
+    });
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <RegisterContainer>
+      <LoginImg src={logo} alt="Imagem da logo AppMasters" />
+      <RegisterTitle>Digite suas informações de cadastro</RegisterTitle>
+
+      <RegisterForm>
+        <RegisterLabel htmlFor="email">E-mail</RegisterLabel>
+        <RegisterInput
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <RegisterLabel htmlFor="password">Senha</RegisterLabel>
+        <RegisterInput
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Digite sua senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <RegisterButton onClick={handleSignOut} className="button">
+          Cadastrar
+        </RegisterButton>
+        <RegisterDiv>
+          <RegisterP>Você já tem uma conta?</RegisterP>
+          <Link to="/auth">Acesse sua conta aqui</Link>
+        </RegisterDiv>
+      </RegisterForm>
+    </RegisterContainer>
+  );
+}
---
+++ src/pages/register/styleRegister.ts
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { theme } from "../../styleGlobal";
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const RegisterImg = styled.img`
+  width: 80px;
+`;
+
+export const RegisterTitle = styled.h2`
+  padding: 20px;
+  text-align: center;
+  color: ${theme.colors.tertiary};
+`;
+
+export const RegisterForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+
+  @media (max-width: 720px) {
+    width: 100%;
+  }
+`;
+
+export const RegisterLabel = styled.label`
+  width: 80%;
+  color: ${theme.colors.tertiary};
+`;
+
+export const RegisterInput = styled.input`
+  width: 80%;
+  height: 40px;
+  border-radius: 10px;
+  padding-left: 10px;
+  border: 1px solid rgb(54, 135, 241);
+  outline: none;
+
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px white inset;
+  }
+`;
+
+export const RegisterA = styled.a`
+    width: 80%;
+    text-decoration: none;
+    color: ${theme.colors.tertiary};
+    cursor: pointer;
+    &:hover {
+        color: ${theme.colors.primary};
+    }
+`;
+
+export const RegisterButton = styled.button`
+  width: 80%;
+  height: 40px;
+  font-size: 20px;
+  color: ${theme.colors.secondary};
+  margin: 10px 0 5px 0;
+  background-color: ${theme.colors.primary};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+export const RegisterDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  width: 80%;
+  gap: 10px;
+
+  @media (max-width: 385px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const RegisterP = styled.p`
+  text-decoration: none;
+  color: ${theme.colors.tertiary};
+`;
